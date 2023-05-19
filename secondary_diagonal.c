52eb55b4c64641fa0096cbc3648ecc6c27330c2d
marufhasan566 <marufhasan.mh1@gmail.com>
2023-05-19 08:58:43 +0600

    Primary Diagonal & Secondary Diagonal

---
+++ secondary_diagonal.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+int main()
+{
+    int row, column;
+    scanf("%d %d", &row, &column);
+    int arr[row][column];
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < column; j++)
+        {
+            scanf("%d", &arr[i][j]);
+        }
+    }
+    int flag = 1;
+    if (row != column)
+    {
+        flag = 0;
+    }
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < column; j++)
+        {
+            if (i + j == row - 1)
+            {
+                continue;
+            }
+            if (arr[i][j] != 0)
+            {
+                flag = 0;
+            }
+        }
+    }
+    if (flag == 1)
+    {
+        printf("Secondary Diagonal\n");
+    }
+    else
+    {
+        printf("Not Diagonal\n");
+    }
+
+    return 0;
+}
